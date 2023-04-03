e475e6d07d69d186d54d4b358401da4bf37b1c41
Saylomon <sergazyelmiraa@gmail.com>
2023-04-03 20:17:22 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
+    <button @click="GetBlockNumber"> Получить номер блока</button>
+    <button @click="getBlock"> Получить Хэш блока</button>
+    <h1>Номер Блока: {{ blockNumber }}</h1>
+    <h1>Хэш Блока: {{ block.hash }}</h1>
     <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
+const Web3 = require('web3')
+const web3 = new Web3('wss://eth-goerli.g.alchemy.com/v2/8H_rc42Uvn9GWrvP5qDSYZumgfFOGfdC')
+export default{
+  data(){
+    return{
+      blockNumber: "",
+      block: {}
+    }
+  },
+  methods: {
+    async GetBlockNumber(){
+      this.blockNumber = await web3.eth.getBlockNumber()
+    },
+    async getBlock(){
+      this.block = await web3.eth.getBlock("latest", true)
+      console.log("QQ")
+    }
+  },
 }
 </script>
 
